a30fdcb362a8d886f7a26b49828e5b7a578d2f30
Thomas Sankara <thomassankara74@gmail.com>
2021-07-27 19:40:52 +0300

    remove semicolons

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const numbers = "0123456789";
-export const lowercaseLetters = "abcdefghijklmnopqrstuvxyz";
-export const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTYUVWXYZ";
-export const specialSymbols = ",./;'[]=`~!@#$%^&*()_-+{}:,.?";
+export const numbers = "0123456789"
+export const lowercaseLetters = "abcdefghijklmnopqrstuvxyz"
+export const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTYUVWXYZ"
+export const specialSymbols = ",./;'[]=`~!@#$%^&*()_-+{}:,.?"
